636b0a0fa319ddf557eadcb5ed5125c3996314da
Mukesh743-kisku <mk0858425@gmail.com>
2026-05-09 09:10:54 +0530

    Create About.py

---
+++ pages/About.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("About Marwari College")
+st.write("Marwari College, Ranchi - Established 1963")
+st.write("This ERP system manages student data, attendance, and results.")
