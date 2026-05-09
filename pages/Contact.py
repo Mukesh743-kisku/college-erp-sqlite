50bd32ec4e438360e7322c38444c7a235d49988b
Mukesh743-kisku <mk0858425@gmail.com>
2026-05-09 09:12:23 +0530

    Create Contact.py

---
+++ pages/Contact.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Contact Us")
+st.write("📍 Address: Lake Road, Ranchi, Jharkhand")
+st.write("📞 Phone: +91-651-220-0123")
+st.write("📧 Email: info@marwaricollege.ac.in")
